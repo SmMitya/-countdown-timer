c6abf2e16d38b453aeb24c2907271fd415766514
SmMitya <Smotrov1991@yandex.ru>
2022-08-27 14:44:05 +0300

    creat timer.js, mindate

--- index.js
+++ index.js
@@ -10,66 +10,17 @@ const completeTimer = document.querySelector('.complete');
 const resetBtn = document.querySelector('#btn-reset');
 const dateNumbers = document.querySelector('.numbers');
 
-class Timer {
-  constructor() {
-    this.deadline = null;
-    this.timerInterval = null;
-    this.activeTimer = false;
-  }
-  // Получаем дату до которой считаем
-  setDeadline() {
-    this.deadline = moment(inputDate.value);
-  }
-  // Состояние таймера 
-  conditionTimer() {
-    if (isNaN(this.deadline)) {
-      this.deadline = null;
-      alert('Введите дату для таймера!');
-      return this.activeTimer;
-    }
+let timer;
+// Минимальная дата выбора
+inputDate.setAttribute('min', moment().format('YYYY-MM-DD hh:mm'));
 
-    this.activeTimer = true;
-  }
-  // Метод разницы дат
-  diffTimer(value) {
-    const nowTime = moment();
-    return this.deadline.diff(nowTime, value);
-  }
-  // Метод очистки таймера
-  clearTimerInterval() {
-    clearInterval(this.timerInterval);
-  }
-  // Метод вывода даты отсчета на экран
-  dateTimerNumbers(days, hours, minutes, seconds) {
-    return `${this.addZero(days)}:${this.addZero(hours)}:${this.addZero(minutes)}:${this.addZero(seconds)}`;
-  }
-  // Метод добавления нуля к числам
-  addZero(value) {
-    return value < 10 ? '0' + value : value;
-  }
-  
-  countdownTimer() {
-    if (this.diffTimer() <= 0) {
-      completeTimer.classList.remove('hide');
-      completeTimer.textContent = `${inputTitle.value} завершился ${this.deadline.format('DD.MM.YYYY hh:mm:ss')}`;
-      this.clearTimerInterval();
-      return;
-    }
-  
-    const days = this.diffTimer('days');
-    const hours = this.diffTimer('hours') % 24; 
-    const minutes = this.diffTimer('minutes') % 60; 
-    const seconds = this.diffTimer('seconds') % 60; 
-  
-    dateNumbers.textContent = this.dateTimerNumbers(days, hours, minutes, seconds);
-  }
-  // Метод через сколько секунд интервал счета
-  setTimerInterval() {
-    this.timerInterval = setInterval(this.countdownTimer.bind(this), 1000);
-  }
+import Timer from './timer.js';
+
+// Сообщение таймера, когда таймер закончился
+function messageComplete(date) {
+  completeTimer.classList.remove('hide');
+  completeTimer.textContent = `${inputTitle.value} завершился ${date.format('DD.MM.YYYY hh:mm:ss')}`;
 }
-
-const timer = new Timer();
 
 // Скрытие стартого окна
 function closeFirstWindow() {
@@ -78,9 +29,6 @@ function closeFirstWindow() {
 
   outputBlock.classList.remove('hide');
   resetBtn.classList.remove('hide');
-
-  timer.countdownTimer();
-  timer.setTimerInterval();
 }
 
 // Скрытие окна с таймером
@@ -98,21 +46,20 @@ function resetTimer() {
   inputDate.value = '';
   inputTitle.value = '';
   completeTimer.textContent = '';
+  dateNumbers.textContent = '0:0:0:0';
+
   localStorage.removeItem('deadlineLocal');
   localStorage.removeItem('titleForTimer');
 
   timer.clearTimerInterval();
-  timer.deadline = null;
-  timer.activeTimer = false;
 
   closeTimerWindow();
 }
 
 function startTimer() {
-  timer.setDeadline();
-  timer.conditionTimer();
+  timer = new Timer(moment(inputDate.value), dateNumbers, messageComplete);
 
-  if (timer.activeTimer === false) {
+  if (timer.init() === false) {
     return;
   }
 
@@ -129,7 +76,8 @@ function getItemLocalStorage() {
     return;
   }
 
-  timer.deadline = moment(deadlineLocal);
+  timer = new Timer(moment(deadlineLocal), dateNumbers, messageComplete)
+  timer.init();
   titleTimer.textContent = inputTitleLocal;
   closeFirstWindow();
 }
